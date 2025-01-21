9ff8ede7c95a06ddbe2c2115ceb27fa741e689f9
Jainee0911 <24bit069@sot.pdpu.ac.in>
2025-01-21 15:54:40 +0530

    Add files via upload

---
+++ lab2(9).py
@@ -0,0 +1,8 @@
+def absoluteValue():
+    num=int(input("enter any number"))
+    if num>=0:
+        print("the absolute value is",num)
+    else:
+        b=num*-1
+        print("the absolute value is",b)
+absoluteValue()
